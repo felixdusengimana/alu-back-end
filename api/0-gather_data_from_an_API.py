9cd67b36ef426d07bd1ef95cf2c7f19a40bf09f5
felixdusengimana <phelixdusengimana@gmail.com>
2024-06-24 12:15:09 +0200

    fix: lints

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -31,19 +31,15 @@ def main(employee_id):
     employee = get_employee_info(employee_id)
     employee_name = employee.get("name")
 
-    employee_todos = get_employee_todos(employee_id)
-    tasks = {todo.get("title"): todo.get("completed")
-             for todo in employee_todos}
+    emp_todos = get_employee_todos(employee_id)
+    tasks = {todo.get("title"): todo.get("completed") for todo in emp_todos}
 
     total_tasks = len(tasks)
-    completed_tasks = len(
-        [completed for completed in tasks.values() if completed])
+    completed_tasks = [completed for completed in tasks.values() if completed]
+    completed_tasks_count = len(completed_tasks)
 
-    print(
-        f"Employee {employee_name} is done with tasks "
-        f"({completed_tasks}/{total_tasks}):"
-    )
-
+    print(f"Employee {employee_name} is done with tasks"
+          f"({completed_tasks_count}/{total_tasks}):")
     for title, completed in tasks.items():
         if completed:
             print(f"\t {title}")
